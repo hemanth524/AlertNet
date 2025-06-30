dd94daad41a14013622c221b7208a7a1ab574d07
hemanth524 <chinthahemanthreddy07@gmail.com>
2025-06-30 16:01:47 +0530

    change

--- server/server.js
+++ server/server.js
@@ -55,11 +55,6 @@ app.get('/', (req, res) => {
   res.send('🚀 AlertNet API is running...');
 });
 
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
 // Socket.IO Setup with Validation
 function iosetup(io) {
